e0be0ae6255e25d9f22111bfe4b638855398e03b
Leilani Hoffmann <167572755+leilanihoffmann26@users.noreply.github.com>
2025-04-07 21:47:29 -0400

    Basic GAN training and test dataset

---
+++ main.py
@@ -0,0 +1,129 @@
+
+# Importing packages
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+from keras import preprocessing
+from keras.models import Sequential
+from keras.layers import Conv2D,Dropout,Dense,Flatten,Conv2DTranspose,BatchNormalization,LeakyReLU,Reshape
+import tensorflow as tf
+
+
+# Input files and save results 
+import os
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+# Load and preprocess data
+train_data = pd.read_csv('/kaggle/input/fashionmnist/fashion-mnist_train.csv')
+X_train = train_data.drop('label',axis=1) # Drop labels
+X_train=X_train.values # Shape is (60000, 784)
+X_train=X_train.reshape(-1,28,28,1) # Reshape to (60000, 28, 28, 1)
+X_train =  X_train.astype('float32') # Normalize values
+X_train = X_train/255
+X_train = X_train*2 - 1. # Change range for tanh activation
+
+# Generator
+generator = Sequential()
+generator.add(Dense(512,input_shape=[100]))
+generator.add(LeakyReLU(alpha=0.2))
+generator.add(BatchNormalization(momentum=0.8))
+generator.add(Dense(256))
+generator.add(LeakyReLU(alpha=0.2))
+generator.add(BatchNormalization(momentum=0.8))
+generator.add(Dense(128))
+generator.add(LeakyReLU(alpha=0.2))
+generator.add(BatchNormalization(momentum=0.8))
+generator.add(Dense(784))
+generator.add(Reshape([28,28,1]))
+generator.summary()
+
+# Discriminator
+discriminator = Sequential()
+discriminator.add(Dense(1,input_shape=[28,28,1]))
+discriminator.add(Flatten())
+discriminator.add(Dense(256))
+discriminator.add(LeakyReLU(alpha=0.2))
+discriminator.add(Dropout(0.5))
+discriminator.add(Dense(128))
+discriminator.add(LeakyReLU(alpha=0.2))
+discriminator.add(Dropout(0.5))
+discriminator.add(Dense(64))
+discriminator.add(LeakyReLU(alpha=0.2))
+discriminator.add(Dropout(0.5))
+discriminator.add(Dense(1,activation='sigmoid'))
+discriminator.summary()
+
+# GAN
+GAN =Sequential([generator,discriminator])
+discriminator.compile(optimizer='adam',loss='binary_crossentropy')
+discriminator.trainable = False
+
+GAN.compile(optimizer='adam',loss='binary_crossentropy')
+GAN.summary()
+
+# GAN parameters
+epochs = 30
+batch_size = 100
+noise_shape=100
+
+# Training GAN
+with tf.device('/gpu:0'):
+ for epoch in range(epochs):
+    print(f"Currently on Epoch {epoch+1}")
+    
+    
+    for i in range(X_train.shape[0]//batch_size):
+        
+        if (i+1)%50 == 0:
+            print(f"\tCurrently on batch number {i+1} of {X_train.shape[0]//batch_size}")
+            
+        noise=np.random.normal(size=[batch_size,noise_shape])
+       
+        gen_image = generator.predict_on_batch(noise)
+        
+        train_dataset = X_train[i*batch_size:(i+1)*batch_size]
+       
+        #training discriminator on real images
+        train_label=np.ones(shape=(batch_size,1))
+        discriminator.trainable = True
+        d_loss_real=discriminator.train_on_batch(train_dataset,train_label)
+        
+        #training discriminator on fake images
+        train_label=np.zeros(shape=(batch_size,1))
+        d_loss_fake=discriminator.train_on_batch(gen_image,train_label)
+        
+        
+        #training generator 
+        noise=np.random.normal(size=[batch_size,noise_shape])
+        train_label=np.ones(shape=(batch_size,1))
+        discriminator.trainable = False
+        
+        d_g_loss_batch =GAN.train_on_batch(noise, train_label)
+        
+        
+        
+       
+    #plotting generated images at the start and then after every 10 epoch
+    if epoch % 10 == 0:
+        samples = 10
+        x_fake = generator.predict(np.random.normal(loc=0, scale=1, size=(samples, 100)))
+
+        for k in range(samples):
+            plt.subplot(2, 5, k+1)
+            plt.imshow(x_fake[k].reshape(28, 28), cmap='gray')
+            plt.xticks([])
+            plt.yticks([])
+
+        plt.tight_layout()
+        plt.show()
+
+        
+        
+print('Training is complete')
+
+# Noise as input
+noise=np.random.normal(size=[10,noise_shape])
+gen_image = generator.predict(noise)
+
